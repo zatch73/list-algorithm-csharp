08c591198c7a451f35efc2a50cf006d1358e7023
zatch73 <marchiolimatheus@gmail.com>
2023-05-31 09:18:35 -0300

    update exercise 1

---
+++ Entities/Lista.cs
@@ -0,0 +1,99 @@
+public class Lista{
+
+    public No inicio;
+    public No fim;
+
+    public Lista(){
+        this.inicio = null;
+        this.fim = null;
+    }
+
+    public void inserirInicio(int value){
+        No novoNo = new(value);
+
+        if(this.inicio == null && this.fim == null){
+            this.inicio = novoNo;
+            this.fim = novoNo;
+            System.Console.WriteLine("Insert in start position: " + this.inicio.valor);
+        }
+        else if(this.inicio != null){
+            novoNo.noProx = this.inicio;
+            this.inicio = novoNo;
+            System.Console.WriteLine("Insert in start position: " + this.inicio.valor);
+        }
+    }
+
+    public void inserirFim(int value){
+        No no = new No(value);
+
+        if(this.inicio == null && this.fim == null){
+            this.inicio = no;
+            this.fim = no;
+            System.Console.WriteLine("Insert in end position: " + this.fim.valor);
+        }else{
+            this.fim.noProx = no;
+            this.fim = no;
+            System.Console.WriteLine("Insert in end position: " + this.fim.valor);
+
+        }
+    }
+
+    public void removerInicio(){
+       if(this.inicio == null && this.fim == null){
+        System.Console.WriteLine("an empty list");
+       }else if(this.inicio != null){
+            this.inicio = this.inicio.noProx;
+            System.Console.WriteLine("Remove of start position: " + this.inicio.valor);
+
+       }
+    }
+
+    public void removerFim(){
+        No noAux = this.inicio;
+       if(this.inicio == null && this.fim == null){
+        System.Console.WriteLine("an empty list");
+       }else if(this.inicio != null){
+
+            while(noAux != null){
+                if(noAux.noProx == this.fim){
+                    System.Console.WriteLine("Remove of end position: " + noAux.valor);
+                    this.fim = noAux.noProx;
+                    noAux.noProx = null;
+
+                }
+                noAux = noAux.noProx;            
+            }
+       }
+    }
+
+    public Boolean consulta(int Valor, ref No noAtual, ref No noAnterior){ 
+        
+        noAtual = this.inicio;
+        noAnterior = null;
+
+        while(noAtual != null){
+
+            if(noAtual.valor == Valor){ //SE ENCONTROU O VALOR
+                return(true);
+            }
+            noAnterior = noAtual;
+            noAtual = noAtual.noProx;
+        }
+    
+        return(false);
+    }
+
+    public void percurso(){
+        
+        No noAux = this.inicio;
+        System.Console.Write("\n[ List: ");
+        while(noAux != null){
+            Console.Write(noAux.valor);
+            if(noAux.noProx != null){
+                Console.Write(" -> ");
+            }
+            noAux = noAux.noProx;            
+        }
+        System.Console.Write(" ]\n\n");
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿namespace ListAlgorithm
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Lista lista = new();
+            lista.inserirInicio(22);
+            lista.inserirInicio(12);
+            lista.inserirInicio(7);
+            lista.inserirFim(10);
+            
+            lista.percurso();
+
+            lista.removerInicio();
+            lista.removerFim();
+            
+            lista.percurso();
+            // No? noAtual = null;
+            // No? noAnterior = null;
+
+            // Boolean encontrou = lista.consulta(10, ref noAtual, ref noAnterior);
+            // if (encontrou)
+            // {
+            //     Console.WriteLine("Encontrou o no de valor: " + noAtual.valor);
+            // }
+            // else
+            // {
+            //     Console.WriteLine("Não encontrou o no procurado!");
+            // }
+        }
+    }
+}
+
